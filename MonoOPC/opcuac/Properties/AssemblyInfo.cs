f4a264f0bf372dac3a79ce6567146f77343f046e
Tuko <tuko.ustx@gmail.com>
2021-01-03 01:00:42 -0600

    Now able to build using .Net Core 5.0

--- MonoOPC/opcuac/Properties/AssemblyInfo.cs
+++ MonoOPC/opcuac/Properties/AssemblyInfo.cs
@@ -4,11 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("opcuac")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +13,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.2.0.*")]
+//[assembly: AssemblyVersion("0.2.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
