d7c3045a6d5b239f905bcca28acbd2294f419169
Tuko <tuko.ustx@gmail.com>
2021-01-14 11:18:00 -0600

    Clean-up script

---
+++ MonoOPC/opcuac/scripts/cleandata.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop"
+$sleep_delay = 60
+
+# $root_path = Split-Path $PSScriptRoot
+# $data_path = Join-Path -Path $root_path  -ChildPath "data"
+$data_path = "C:\gitprj\mopcua\MonoOPC\opcuac\bin\Debug\data"
+Write-Host $data_path
+while($true)
+{
+    $logs = @(Get-Item  (Join-Path -Path $data_path -ChildPath "*.txt") | Sort-Object)
+    if ($logs.Length -gt 10) 
+    {
+        Write-Host Deleting : $logs[0].FullName
+        Remove-Item $logs[0].FullName
+        break
+    } else {
+        Write-Host  [$(Get-Date)] - Sleeping for $sleep_delay seconds
+        Start-Sleep -Seconds $sleep_delay
+    }
+}
