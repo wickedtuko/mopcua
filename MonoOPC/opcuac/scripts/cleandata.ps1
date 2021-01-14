4fb8db734eb2b58222d97abcd66aeb9763c15397
Tuko <tuko.ustx@gmail.com>
2021-01-14 11:22:30 -0600

    Make max_files variable

--- MonoOPC/opcuac/scripts/cleandata.ps1
+++ MonoOPC/opcuac/scripts/cleandata.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 $sleep_delay = 60
-
+$max_files = 10
 # $root_path = Split-Path $PSScriptRoot
 # $data_path = Join-Path -Path $root_path  -ChildPath "data"
 $data_path = "C:\gitprj\mopcua\MonoOPC\opcuac\bin\Debug\data"
@@ -8,12 +8,12 @@ Write-Host $data_path
 while($true)
 {
     $logs = @(Get-Item  (Join-Path -Path $data_path -ChildPath "*.txt") | Sort-Object)
-    if ($logs.Length -gt 10) 
+    if ($logs.Length -gt $max_files) 
     {
         Write-Host Deleting : $logs[0].FullName
         Remove-Item $logs[0].FullName
     } else {
-        Write-Host  [$(Get-Date)] - Sleeping for $sleep_delay seconds
+        Write-Host  "[$(Get-Date)] - Files within $max_files, sleeping for $sleep_delay seconds"
         Start-Sleep -Seconds $sleep_delay
     }
 }
